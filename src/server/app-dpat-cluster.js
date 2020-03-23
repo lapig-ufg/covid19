badd4b1b91d6d35476b2d1cb6aa35c3d1462fcd3
Luiz <luizmlpascoal@gmail.com>
2020-03-23 10:43:09 -0300

    alter worker size

--- src/server/app-dpat-cluster.js
+++ src/server/app-dpat-cluster.js
@@ -1,6 +1,7 @@
 var cluster = require('cluster');
 var http = require('http');
-var numCPUs = require('os').cpus().length;
+// var numCPUs = require('os').cpus().length;
+var numCPUs = 8;
 
 if (cluster.isMaster) {
   if(process.env.NODE_ENV == 'dev') {
