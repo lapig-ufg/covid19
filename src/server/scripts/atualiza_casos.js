f6ffa2b4efee23eefbaaee1976aa072a8df0d903
renatowow14 <renatogomes256@gmail.com>
2020-03-31 18:50:52 -0300

    update fix

--- src/server/scripts/atualiza_casos.js
+++ src/server/scripts/atualiza_casos.js
@@ -13,65 +13,63 @@ const insertRow = 'INSERT INTO casos(cd_geocmu, data, confirmados) VALUES($1,$2,
 const dropView = 'DROP VIEW municipios_casos'
 var newView = "CREATE OR REPLACE VIEW municipios_casos AS SELECT m.*, p.estp_2019 AS pop_2019, c.confirmados, c.data, c.suspeitos, c.descartados, c.obitos \
 FROM municipios m \
-  INNER populacao p ON m.cd_geocmu = p.cd_geocmu \
+  INNER JOIN populacao p ON m.cd_geocmu = p.cd_geocmu \
   LEFT JOIN casos c ON m.cd_geocmu = c.cd_geocmu \
 WHERE m.cd_geocmu <> '52' AND c.data IS NULL OR c.data = "
 
 fs.createReadStream(csvFilepath)
-	.pipe(csv())
-	.on('data', (row) => {
-		csvRows.push(row)
-	})
-	.on('end', () => {
-		
-		(async () => {
+    .pipe(csv())
+    .on('data', (row) => {
+        csvRows.push(row)
+    })
+    .on('end', () => {
+        
+        (async () => {
   
-		  const client = await pool.connect()
-		  try {
-		    await client.query('BEGIN')
-		    
-		    const lastDateResul = await client.query(lastDateQuery)
-		    const lastDate = lastDateResul.rows[0]['last_date']
+          const client = await pool.connect()
+          try {
+            await client.query('BEGIN')
+            
+            const lastDateResul = await client.query(lastDateQuery)
+            const lastDate = lastDateResul.rows[0]['last_date']
 
-		    var newLastDate = undefined
+            var newLastDate = undefined
 
-		    for(i in csvRows) {
-		    	var row = csvRows[i]
-				var rowDate = new Date(row.data)
+            for(i in csvRows) {
+                var row = csvRows[i]
+                var rowDate = new Date(row.data)
 
-		    	if (rowDate > lastDate) {
-			    	
-			    	if (newLastDate == undefined || newLastDate < rowDate) {			    		
-			    		newLastDate = row.data
-					}
-					
-					
-			    	var rowValues = [row.cd_geocmu, row.data, row.confirmados, row.suspeitos, row.descartados, row.obitos] 
-		  			const res = await client.query(insertRow, rowValues)
-		  			console.log(res.rowCount + ' inserted.')
-		    	} else {
-		    		console.log('Duplicated register ignored.')
-		    	}
+                if (rowDate > lastDate) {
+                    
+                    if (newLastDate == undefined || newLastDate < rowDate) {                        
+                        newLastDate = row.data
+                    }
+                    var rowValues = [row.cd_geocmu, row.data, row.confirmados] 
+                    const res = await client.query(insertRow, rowValues)
+                    console.log(res.rowCount + ' inserted.')
+                } else {
+                    console.log('Duplicated register ignored.')
+                }
 
-		    }
+            }
 
-		    console.log((newLastDate != undefined), newLastDate)
-		    if (newLastDate != undefined) {
-		    	await client.query(dropView)
-		    	await client.query(newView + "'" + newLastDate + "'")
-		    	console.log('View municipios_casos updated')
-		    }
+            console.log((newLastDate != undefined), newLastDate)
+            if (newLastDate != undefined) {
+                await client.query(dropView)
+                await client.query(newView + "'" + newLastDate + "'")
+                console.log('View municipios_casos updated')
+            }
 
-		    console.log("Doing commit")
-		    await client.query('COMMIT')
+            console.log("Doing commit")
+            await client.query('COMMIT')
 
-		  } catch (e) {
-		    console.log("Doing rollback")
-		    await client.query('ROLLBACK')
-		    throw e
-		  } finally {
-		    client.release()
-		  }
-		})().catch(e => console.error(e.stack))
+          } catch (e) {
+            console.log("Doing rollback")
+            await client.query('ROLLBACK')
+            throw e
+          } finally {
+            client.release()
+          }
+        })().catch(e => console.error(e.stack))
 
-});	
+});
