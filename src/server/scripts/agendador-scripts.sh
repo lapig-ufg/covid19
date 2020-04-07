1246000cdc8a08c84a5f3055fb23571a6e7489e5
renatowow14 <renatogomes256@gmail.com>
2020-04-07 00:47:19 -0300

    Update dos horários

--- src/server/scripts/agendador-scripts.sh
+++ src/server/scripts/agendador-scripts.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #sleep 1h
-#sleep 5m
+
 while :; do
 
 comando=$(date +%H:%M)
 
-if [[ "$comando" == '18:00' ]] || [[ "$comando" == '06:00' ]];then
+if [[ "$comando" == '18:00' ]] || [[ "$comando" == '06:00' ]] || [[ "$comando" == '10:00' ]];then
                    sleep 5
                    echo -n "Atualizando Plataforma!!"
-		   cd /APP/covid19/src/server/scripts/
+		               cd /APP/covid19/src/server/scripts/
                    /APP/covid19/src/server/scripts/atualizacao_automatica_casos.sh
                    sleep 3
                    clear
               else
               	  sleep 5 
                   clear
-                  echo -n "Ainda nao sao 6 da tarde! e Ainda nao sao 6 da manha!"
+                  echo -n "Ainda nao sao 6:00 da tarde! e Ainda nao sao 6:00 da manha! e Ainda nao sao 10:00 da manha!"
                   sleep 3
                   clear
               fi
