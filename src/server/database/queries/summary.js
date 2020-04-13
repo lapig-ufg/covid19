9af1b33feaccae0060b8b33bd3a37d72789bdce6
Tharles Andrade <irtharles@gmail.com>
2020-04-13 10:19:36 -0300

    Fixed count summary

--- src/server/database/queries/summary.js
+++ src/server/database/queries/summary.js
@@ -20,11 +20,13 @@ module.exports = function (app) {
             filter = "cd_geocmu = '" + cd_geocmu + "' "
         }
 
+        // sql: "with cte as (select (select nome from municipios where cd_geocmu = '" + cd_geocmu + "'), max(confirmados) as confirmados, (select max(suspeitos) as suspeitos from casos group by data order by data desc limit 1) as suspeitos, max(obitos) as obitos from casos where data = (select max(data) from casos) AND " + filter + " group by cd_geocmu) " +
+        // "select nome, sum (confirmados) as confirmados, max(suspeitos) as suspeitos, sum(obitos) as obitos from cte group by 1;"
+
         return [
             {
                 id: 'resumed_indicators',
-                sql: "with cte as (select (select nome from municipios where cd_geocmu = '" + cd_geocmu + "'), max(confirmados) as confirmados, (select max(suspeitos) as suspeitos from casos group by data order by data desc limit 1) as suspeitos, max(obitos) as obitos from casos where data = (select max(data) from casos) AND " + filter + " group by cd_geocmu) " +
-                    "select nome, sum (confirmados) as confirmados, max(suspeitos) as suspeitos, sum(obitos) as obitos from cte group by 1;"
+                sql: "SELECT SUM(confirmados) as confirmados, SUM(suspeitos) as suspeitos, SUM(obitos) as obitos FROM casos WHERE data = (SELECT MAX(data) FROM casos) AND " + filter +"",
             },
             {
                 id: 'last_update',
