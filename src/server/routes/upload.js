789235f36ba571803b33fed4bc351994b313a29c
Tharles Andrade <irtharles@gmail.com>
2020-05-04 15:00:01 -0300

    Remove upload from project.

--- src/server/routes/upload.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-
-	var filesAccepted = app.middleware.file;
-	var uploader = app.controllers.uploader;
-
-	app.post('/service/upload/spatial-file', filesAccepted, uploader.getGeoJson);
-}
