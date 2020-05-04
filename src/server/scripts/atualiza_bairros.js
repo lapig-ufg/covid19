6b1a406cc74843887d06ff49c9e8826fcbe9883c
Renato Gomes <renatogomes256@gmail.com>
2020-05-04 02:19:23 -0300

    Script Atualizacao de Bairros

--- src/server/scripts/atualiza_bairros.js
+++ src/server/scripts/atualiza_bairros.js
@@ -5,7 +5,7 @@ const {
 const csv = require('csv-parser');
 const fs = require('fs');
 
-var config = require('../config.js')()
+var config = require('../configScript.js')()
 var pool = new Pool(config['pg'])
 
 var csvRows = []
