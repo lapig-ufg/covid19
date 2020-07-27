c2ed4c4f982d5f2ffce4299f104ffd4ad5a4633d
renatowow14 <renatogomes256@gmail.com>
2020-07-27 12:45:34 -0300

    Update atualizacao_automatica_estatisticas.sh

--- src/server/scripts/atualizacao_automatica_estatisticas.sh
+++ src/server/scripts/atualizacao_automatica_estatisticas.sh
@@ -5,7 +5,16 @@ BASELOCAL='/data/containers/APP_COVID19/APP/covid19/src/server/scripts'
 DATA=`date +%d-%m-%Y-%H.%M`
 DATA02=`date +%d-%m-%Y`
 
-apt-get -y install pv
+#Include telegram chat id and bot token ID here
+chat_id="-427575689"
+TOKEN="1370924692:AAFd-MW3vei_24_HkQxcwHPYtKJmKnFT2so"
+
+function UPDATE-COVID19-ALERT
+{
+curl --silent --output /dev/null  "https://api.telegram.org/bot$TOKEN/sendMessage?chat_id=$chat_id&text=$SUBJECT" > /dev/null 
+}
+
+apt-get -y install pv >> /dev/null
 
 pip install gspread >> /dev/null
 
@@ -86,9 +95,9 @@ fi
 if [ -d $BASESTORAGE/projecao_luisa_hospitalizados-tiles ];then
 
         echo "Arquivos existem apagando!"
-    	cd $BASESTORAGE
-    	cd projecao_luisa_hospitalizados-tiles
-    	rm -rfv *
+        cd $BASESTORAGE
+        cd projecao_luisa_hospitalizados-tiles
+        rm -rfv *
 else
         echo "Arquivos projecao_luisa_hospitalizados-tiles nao existem!"
 fi
@@ -107,9 +116,9 @@ fi
 if [ -d $BASESTORAGE/projecao_luisa_recuperados-tiles];then
 
       echo "Arquivos existem apagando!"
-    	cd $BASESTORAGE
-    	cd projecao_luisa_recuperados-tiles
-    	rm -rfv *
+        cd $BASESTORAGE
+        cd projecao_luisa_recuperados-tiles
+        rm -rfv *
 else
         echo "Arquivos projecao_luisa_recuperados-tiles nao existem!"
 fi
@@ -117,27 +126,24 @@ fi
 if [ -d $BASESTORAGE/covid_medias_moveis-tiles ];then
 
         echo "Arquivos existem apagando!"
-    	cd $BASESTORAGE
-    	cd covid_medias_moveis-tiles
-    	rm -rfv *
+        cd $BASESTORAGE
+        cd covid_medias_moveis-tiles
+        rm -rfv *
 else
         echo "Arquivos covid_medias_moveis-tiles nao existem!"
 fi
 
-
-clear
-echo "Apagando Rastros!"
-sleep 2
-
-cd $BASELOCAL
-
 clear
 echo -n -e "Rotina Concluida!"
 
-cd $BASELOCAL
+echo -e "Rotina Concluida em $DATA \n\n" >> /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualizacao_de_dados.log
 
-echo -e "LOG DE EXECUÇÃO DA ATUALIZAÇÃO/n/n"
+echo -n "Enviando Mensagem ao Telegram"
+
+SUBJECT="✅ UPDATE atualizacao_automatica_estatisticas.sh on 200.137.217.159 Server Time : $(date +" %d %b %Y %T")"
+UPDATE-COVID19-ALERT
+
+clear
+echo -e "LOG DE EXECUÇÃO DA ATUALIZAÇÃO: \n\n"
 
 cat /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualizacao_de_dados.log
-
-echo -e "Rotina Concluida em $DATA/n/n" >> /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualizacao_de_dados.log
