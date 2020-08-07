f596c1ad5279075c0579809dec58243ab6e46ddc
renatowow14 <renatogomes256@gmail.com>
2020-08-07 17:04:06 -0300

    Update atualizacao_automatica_bairros.sh

--- src/server/scripts/atualizacao_automatica_bairros.sh
+++ src/server/scripts/atualizacao_automatica_bairros.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+#Script de Atualização de Casos por Bairros.
 
 BASESTORAGE='/data/storage/ows-cache/layers'
-BASELOCAL='/data/containers/APP_COVID19/APP/covid19/src/server/scripts'
+BASELOCAL='/data/containers/APP_COVID19/APP/covid19/src/server/scripts/'
 START_DATE=`date +%d-%m-%Y-%H.%M.%S`
-
-echo -e "\n\nRotina atualizacao_automatica_casos.sh iniciada em: $START_DATE" | tee -a /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualizacao_de_dados.log
 
 #Include telegram chat id and bot token ID here
 chat_id="-427575689"
@@ -15,83 +14,67 @@ function UPDATE-COVID19-ALERT
 curl --silent --output /dev/null  "https://api.telegram.org/bot$TOKEN/sendMessage?chat_id=$chat_id&text=$SUBJECT" > /dev/null 
 }
 
+echo -e "\n\nRotina atualizacao_automatica_bairros.sh iniciada em: $START_DATE" | tee -a /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualizacao_de_dados.log
 
-apt-get -y install pv >> /dev/null
-
-pip install gspread >> /dev/null
-
-pip3 install pydrive >> /dev/null
+echo -n -e "\nBaixando Planilhas do GoogleSheets!\n"
+sleep 2
 
 cd $BASELOCAL
 
-#Step 0
+python2 download_planilha_casos_bairros.py
 
-clear
-echo -n -e "Atualizando Planilhas Projections!"
+mv 1l_3ZlgEBdd53BZFhGktgzvnuze7s3r0QGwVQomfo1eU-worksheet0.csv bairros_casos.csv
+mv 1H7k1RwWqUCcqkMlr7puEx2bgftsrLk7MwEpXNkuN_F4-worksheet0.csv bairros_obitos.csv
+
+echo -n -e "\nAtualizando O Banco de Dados para Casos!\n"
 sleep 2
 
-wget 'https://covidgoias.ufg.br/service/indicators/projections?cd_geocmu=5211909&lang=pt-br%27' -O Jatai.txt
+node atualiza_bairros.js  | tee /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualiza_bairros.log
 
-wget 'https://covidgoias.ufg.br/service/indicators/projections?cd_geocmu=5218805&lang=pt-br%27' -O RioVerde.txt
-
-wget 'https://covidgoias.ufg.br/service/indicators/projections?cd_geocmu=5213103&lang=pt-br%27' -O Mineiros.txt
-
-python3 upload_googledrive.py
-
-#Step 2
-cd $BASELOCAL
-
-clear
-echo -n -e "Populando banco de dados!"
+echo -n -e "\n\n[ Atualizando O Banco de Dados para Óbitos! ] "
 sleep 2
 
-clear
-node atualiza_casos_municipios.js | tee /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualiza_casos_municipios.log
-sleep 2
-clear
+node atualiza_bairros_obitos.js  | tee /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualiza_bairros_obitos.log
 
-
-clear
-echo -n -e "Excluindo Cache!"
+echo -n -e "\nLimpando Cache dos Dados!\n"
 sleep 2
 
-if [ -d $BASESTORAGE/covid19_municipios_casos_utfgrid-tiles ];then    
+
+if [ -d $BASESTORAGE/casos_por_bairro_covid-tiles ];then    
        
         echo "Arquivos existem apagando!"
         cd $BASESTORAGE
-        cd covid19_municipios_casos_utfgrid-tiles 
+        cd casos_por_bairro_covid-tiles 
         rm -rfv *
     
-else	
+else    
         echo "Arquivos nao existem!" 
 fi
 
-sleep 3
-if [ -d $BASESTORAGE/covid19_municipios_casos-tiles ];then    
+if [ -d $BASESTORAGE/obitos_por_bairro_covid-tile ];then    
        
         echo "Arquivos existem apagando!" 
-    	cd $BASESTORAGE
-    	cd covid19_municipios_casos-tiles
-    	rm -rfv * 
-else	
+        cd $BASESTORAGE
+        cd obitos_por_bairro_covid-tiles
+        rm -rfv * 
+else    
         echo "Arquivos nao existem!" 
 fi
 
 clear
 echo -n -e "Rotina Concluida!"
 
-cd $BASELOCAL
-
 clear
 END_DATE=`date +%d-%m-%Y-%H.%M.%S`
-echo "Rotina atualizacao_automatica_casos.sh Concluida em $END_DATE" | tee -a  /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualizacao_de_dados.log
+
+echo "Rotina atualizacao_automatica_bairros.sh Concluida em $END_DATE" | tee -a  /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualizacao_de_dados.log
 
 echo -n "Enviando Mensagem ao Telegram"
 
-SUBJECT="✅ UPDATE atualizacao_automatica_casos.sh on 200.137.217.159 Server Time : $(date +" %d %b %Y %T")"
+SUBJECT="✅ UPDATE atualizacao_automatica_bairros.sh on 200.137.217.159 Server Time : $(date +" %d %b %Y %T")"
 UPDATE-COVID19-ALERT
 
-
+clear
 echo -e "LOG DE EXECUÇÃO DA ATUALIZAÇÃO: \n\n"
 
 cat /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualizacao_de_dados.log
