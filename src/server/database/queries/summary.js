2384106cc7d21fbd7f00f9b83471efa11841cf49
Tharles Andrade <irtharles@gmail.com>
2020-08-11 04:12:18 -0300

    fixed charts

--- src/server/database/queries/summary.js
+++ src/server/database/queries/summary.js
@@ -10,12 +10,14 @@ module.exports = function (app) {
 
         var filter = ""
         var cd_geocmu = params['cd_geocmu']
-
+        var f = "0"
         if (cd_geocmu == 52) {
             filter = "cd_geocmu <> '52' AND cd_geocmu <> '5300108' "
+            f = "222"
         }
         else {
             filter = "cd_geocmu = '" + cd_geocmu + "' "
+            f = cd_geocmu
         }
 
         // sql: "with cte as (select (select nome from municipios where cd_geocmu = '" + cd_geocmu + "'), max(confirmados) as confirmados, (select max(suspeitos) as suspeitos from casos group by data order by data desc limit 1) as suspeitos, max(obitos) as obitos from casos where data = (select max(data) from casos) AND " + filter + " group by cd_geocmu) " +
@@ -24,7 +26,7 @@ module.exports = function (app) {
         return [
             {
                 id: 'resumed_indicators',
-                sql: "SELECT SUM(recuperados) as recuperados, SUM(confirmados) as confirmados, SUM(suspeitos) as suspeitos, SUM(obitos) as obitos FROM casos WHERE data = (select max(data) from casos) AND " + filter +"",
+                sql: "SELECT (select max(recuperados) as recuperados from casos where cd_geocmu = '" + f + "' and data = (select max(data) from casos)), SUM(confirmados) as confirmados, SUM(suspeitos) as suspeitos, SUM(obitos) as obitos FROM casos WHERE data = (select max(data) from casos) AND " + filter + "",
             },
             {
                 id: 'last_update',
