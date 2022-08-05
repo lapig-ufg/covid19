d722f78977f23e3e76b3946bd198426c283a9f6f
Renato Gomes <renatogomessilverio@gmail.com>
2022-08-05 14:08:19 -0300

    Update atualizacao_automatica_casos.sh

--- src/server/scripts/atualizacao_automatica_casos.sh
+++ src/server/scripts/atualizacao_automatica_casos.sh
@@ -6,13 +6,14 @@ START_DATE=`date +%d-%m-%Y-%H.%M.%S`
 
 echo -e "\n\nRotina atualizacao_automatica_casos.sh iniciada em: $START_DATE" | tee -a /data/containers/APP_COVID19/APP/covid19/src/server/scripts/logs/atualizacao_de_dados.log
 
-#Include telegram chat id and bot token ID here and URL variable
-chat_id="-427575689"
-TOKEN="1370924692:AAFd-MW3vei_24_HkQxcwHPYtKJmKnFT2so"
-URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-
 function UPDATE-COVID19-ALERT
 {
+
+#Include telegram chat id and bot token ID here and URL variable
+source ../.env
+chat_id="$chat_id"
+TOKEN="$token"
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
 curl -s -X POST $URL -d chat_id="$chat_id" -d text="$SUBJECT" > /dev/null 2>&1
 
